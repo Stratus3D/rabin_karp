ae4d8c94968875b3dad6cf73bdf1f4dd4224cc83
Stratus3D <admin@stratus3d.com>
2015-07-03 09:36:29 -0400

    WIP: Still working on search function.

--- src/rabin_karp.erl
+++ src/rabin_karp.erl
@@ -1,13 +1,27 @@
 -module(rabin_karp).
 
+-define(LONG_PRIME, 33554393).
+-define(DEFAULT_RADIX, 32).
+
 -export([search/2, search/4]).
 
 % Rabin-Karp search functions
 search(Text, Pattern) ->
-    search(Text, Pattern, 0, 0).
+    search(Text, Pattern, ?DEFAULT_RADIX, ?LONG_PRIME).
 
-search(Text, Pattern, D, Q) ->
+
+search(Text, Pattern, _Radix, _Prime) when length(Pattern) > length(Text) ->
+    [];
+search(Text, Pattern, Radix, Prime) ->
+    TextLength = length(Text),
+    PatternLength = length(Pattern),
+
+    % Prepare pattern
+    {P, T} = preprocess_pattern(Text, Pattern, Radix, Prime),
     % TODO: Complete implementation.
+    not_implemented.
+
+preprocess_pattern(Text, Pattern, Radix, Prime) ->
     not_implemented.
 
 % Hash function
